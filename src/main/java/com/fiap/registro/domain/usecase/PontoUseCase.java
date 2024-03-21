ea337fa3083b0af3e2c7e1cffb99337c3a4a9faf
gabriel <gabrielalmeidads@gmail.com>
2024-03-21 00:32:31 -0300

    FEAT Adicionando integração com SQS

--- src/main/java/com/fiap/registro/domain/usecase/PontoUseCase.java
+++ src/main/java/com/fiap/registro/domain/usecase/PontoUseCase.java
@@ -2,8 +2,10 @@ package com.fiap.registro.domain.usecase;
 
 import com.fiap.registro.domain.model.Ponto;
 import com.fiap.registro.domain.ports.in.IPontoUseCasePort;
+import com.fiap.registro.domain.ports.out.IAtualizaRegistroPontoQueuePort;
 import com.fiap.registro.domain.ports.out.IPontoRepositoryPort;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,14 +13,21 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class PontoUseCase implements IPontoUseCasePort {
     private final IPontoRepositoryPort repository;
+    private final IAtualizaRegistroPontoQueuePort atualizaRegistroPontoQueuePort;
 
     @Override
+    @Transactional
     public Optional<Ponto> registrar(String usuario, String matricula, String email) {
-        return repository.registrar(usuario, matricula, email);
+        Optional<Ponto> pontoRegistrado = repository.registrar(usuario, matricula, email);
+        if (pontoRegistrado.isPresent()) {
+            atualizaRegistroPontoQueuePort.publish(pontoRegistrado.get().toJsonStr());
+        }
+        return pontoRegistrado;
     }
 
     @Override
     public List<Ponto> obterRegistrosPorUsuario(String usuario) {
         return repository.obterRegistrosPorUsuario(usuario);
     }
+
 }
